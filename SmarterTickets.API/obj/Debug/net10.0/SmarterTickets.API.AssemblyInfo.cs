b2dd79f55dc709d248586c5eb3770d8c2a04a74a
Don <donde@Dons-MacBook-Air.local>
2026-03-13 14:04:41 -0700

    Top bar, admin functionality, logout buttons, Profile page

--- SmarterTickets.API/obj/Debug/net10.0/SmarterTickets.API.AssemblyInfo.cs
+++ SmarterTickets.API/obj/Debug/net10.0/SmarterTickets.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmarterTickets.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e874ea4aa4cb0430e56c8342bf66b109f50711f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db11617ac167b211d900393ea31cd5493e6f318d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmarterTickets.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmarterTickets.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
